454cc98e0c2a97cf725213b7a126918914c6aaa5
Vanessa Steffen <vanessasteffen186@gmail.com>
2022-04-26 10:11:11 -0300

    .

---
+++ ex12.js
@@ -0,0 +1,15 @@
+let a = new Boolean(true);
+console.log(a);
+
+let b = new Boolean(false);
+console.log(b);
+
+let c = typeof new Boolean(true);
+console.log(c);
+
+let condition = true;
+if (condition){
+    console.log('a condição é verdadeira')
+}else{
+    console.log('a condição é falsa');
+}
